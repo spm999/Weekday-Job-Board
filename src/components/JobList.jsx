9e6428632749c33684aede76f1e5227a926c69fe
Developers Today <mshari7185@gmail.com>
2024-05-07 12:48:51 +0530

    fetch data from api using axios

---
+++ src/components/JobList.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const JobList = () => {
+  const [allJobs, setAllJobs] = useState([]);
+  const [filteredJobs, setFilteredJobs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1); // Track the current page number
+
+  useEffect(() => {
+    fetchData();
+  }, [page]); // Fetch data whenever the page changes
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.post("https://api.weekday.technology/adhoc/getSampleJdJSON", {
+        limit: 10,
+        offset: (page - 1) * 10 // Calculate the offset based on the current page
+      });
+      const result = response.data;
+      setAllJobs(prevJobs => [...prevJobs, ...result.jdList]); // Append new jobs to the existing list
+      setLoading(false);
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+      setLoading(false);
+    }
+  };
+
+  
+
+  return (
+    <div className='job-list-container'>
+
+    </div>
+  );
+};
+
+export default JobList;
